f2ba26fb437918d6444255411fe2ced8dd09a508
Zivile <to@zivile.dev>
2024-07-11 11:01:04 +0300

    cv link

---
+++ js/components/menu.js
@@ -0,0 +1,8 @@
+function menuLinks() {
+    var menuLink = document.getElementById('myLinks');
+    if (menuLink.style.display === 'block') {
+        menuLink.style.display = 'none';
+    } else {
+        menuLink.style.display = 'block';
+    }
+}
